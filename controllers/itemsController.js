3eb177a7793cd54a9319439942be75a712d2d1ef
Strallia Chao <134454347+strallia@users.noreply.github.com>
2024-08-13 23:31:50 -0700

    feat: Add ability to update an item

--- controllers/itemsController.js
+++ controllers/itemsController.js
@@ -11,4 +11,18 @@ const deleteItem = (req, res) => {
   res.redirect("/items");
 };
 
-module.exports = { getAllItems, deleteItem };
+const getUpdateItemForm = (req, res) => {
+  const itemID = req.params.id;
+  const item = items.find((item) => item.id == itemID);
+  res.render("updateItemForm", { item });
+};
+
+const putUpdateItem = (req, res) => {
+  const itemID = req.params.id;
+  const { name } = req.body;
+  const item = items.find((item) => item.id == itemID);
+  item.name = name;
+  res.redirect(`/items`);
+};
+
+module.exports = { getAllItems, deleteItem, getUpdateItemForm, putUpdateItem };
--- routes/itemsRoutes.js
+++ routes/itemsRoutes.js
@@ -4,5 +4,7 @@ const itemsController = require("../controllers/itemsController");
 
 itemsRouter.get("/", itemsController.getAllItems);
 itemsRouter.delete("/:id", itemsController.deleteItem);
+itemsRouter.get("/:id/update", itemsController.getUpdateItemForm);
+itemsRouter.put("/:id/update", itemsController.putUpdateItem);
 
 module.exports = itemsRouter;
