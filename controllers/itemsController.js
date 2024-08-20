da8d8f7ea3875f77a4de3b050341fa93b8fe7d12
Strallia Chao <134454347+strallia@users.noreply.github.com>
2024-08-19 22:32:21 -0700

    feat: Add server side validation to item forms

--- controllers/itemsController.js
+++ controllers/itemsController.js
@@ -1,6 +1,14 @@
-let { items, runningItemID } = require("../utils/items");
-let { categories } = require("../utils/categories");
 const db = require("../db/queries");
+const { body, validationResult } = require("express-validator");
+
+const validateItemForm = [
+  body("title")
+    .notEmpty()
+    .withMessage("Title can not be empty.")
+    .isLength({ max: 30 })
+    .withMessage("Title can not be more than 30 characters"),
+  body("categoryID").notEmpty().withMessage("Category can not be empty"),
+];
 
 const getAllItems = async (req, res) => {
   const items = await db.getAllItems();
@@ -12,11 +20,22 @@ const getAddItemForm = async (req, res) => {
   res.render("addItemForm", { categories });
 };
 
-const postAddItem = async (req, res) => {
-  const { title, categoryID } = req.body;
-  await db.addItem(title, categoryID);
-  res.redirect("/items");
-};
+const postAddItem = [
+  validateItemForm,
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      const categories = await db.getAllCategories();
+      return res.render("addItemForm", {
+        categories,
+        errors: errors.array(),
+      });
+    }
+    const { title, categoryID } = req.body;
+    await db.addItem(title, categoryID);
+    res.redirect("/items");
+  },
+];
 
 const deleteItem = async (req, res) => {
   const itemID = req.params.id;
@@ -31,12 +50,25 @@ const getUpdateItemForm = async (req, res) => {
   res.render("updateItemForm", { item, categories });
 };
 
-const putUpdateItem = async (req, res) => {
-  const itemID = req.params.id;
-  const { title, categoryID } = req.body;
-  await db.updateItem(itemID, title, categoryID);
-  res.redirect(`/items`);
-};
+const putUpdateItem = [
+  validateItemForm,
+  async (req, res) => {
+    const itemID = req.params.id;
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      const item = await db.findItem(itemID);
+      const categories = await db.getAllCategories();
+      return res.render("updateItemForm", {
+        item,
+        categories,
+        errors: errors.array(),
+      });
+    }
+    const { title, categoryID } = req.body;
+    await db.updateItem(itemID, title, categoryID);
+    res.redirect(`/items`);
+  },
+];
 
 module.exports = {
   getAllItems,
