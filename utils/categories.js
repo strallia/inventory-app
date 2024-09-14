7b4b0eb34927563e187d679a2d9cc42d72b13cea
Strallia Chao <134454347+strallia@users.noreply.github.com>
2024-09-14 15:17:18 -0700

    chore: Remove dead code used prior to database integration

--- utils/categories.js
+++
@@ -1,9 +0,0 @@
-let categories = [
-  { id: "1", name: "wheels" },
-  { id: "2", name: "roller skates" },
-  { id: "3", name: "brands" },
-];
-
-let runningCategoryID = "4";
-
-module.exports = { categories, runningCategoryID };
